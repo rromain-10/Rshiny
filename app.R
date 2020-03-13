5e8e5834c12c2c70170ac691a48c36a372213888
David King <davidcking.inbox@gmail.com>
2020-03-13 13:28:44 -0600

    changes we just did

--- app.R
+++ app.R
@@ -4,12 +4,16 @@ library(reshape2)
 
 #inputs and outputs
 ui<- fluidPage("ARPE19 cancer cell progression through the Ras pathway",
-               fileInput(inputId = "Counts", label= "Upload data file",
+               fileInput(inputId = "counts", label= "Upload data file",
                          multiple = , accept = NULL, width = NULL,
                          buttonLabel = "Browse..."),
-               checkboxGroupInput(inputId = "Gene", label = " Choose Gene of Interest",
-                           c("PPP1CB", "PPP2R5C", "BUB1B","AURKB" ,"SKA1")),
-               checkboxGroupInput(inputId = "Sample", label = " Choose sample",
+               checkboxGroupInput(inputId = "gene", 
+                                  label = " Choose Gene of Interest",
+                                  unlist(strsplit('AURKB BUB1 BUB1B NUF2 PPP1CA PPP1CB PPP1CC PPP2R2A PPP2R2A.1 PPP2R5A PPP2R5B PPP2R5C PPP2R5D PPP2R5E SGO1 SKA1 SKA2',
+                                                   split=' ')
+                                         )
+                                  ),
+               checkboxGroupInput(inputId = "sample", label = " Choose sample",
                            c("ARPE19", "T53D4", "RasV12","Aktmyr" ,"MekDD")),
                submitButton(text = "Apply Changes", icon = NULL, width = NULL),
                mainPanel(
@@ -21,33 +25,33 @@ ui<- fluidPage("ARPE19 cancer cell progression through the Ras pathway",
 #server
 server<- function(input, output) {
   output$boxplot<- renderPlot({ 
-    req(input$Counts)
+    req(input$counts)
     tryCatch(
       {
-        df<- read.csv(input$Counts$datapath
-                      )
+        df<- read.csv("DEseq2 means_goi_ncounts.csv")
       },
       error = function (e){
         stop(safeError(e))
       }
     )
-   #return(df)
-    melted = melt(df)
-    boxplot(melted$value ~ melted$Gene, xaxt="n")
-    text(x = 1:length(levels(melted$Gene)),
-         ## Move labels to just below bottom of chart.
-         y = par("usr")[3] - 0.90,
-         ## Use names from the data list.
-         labels = levels(melted$Gene),
-         ## Change the clipping region.
-         xpd = NA,
-         ## Rotate the labels by 35 degrees.
-         srt = 35,
-         ## Adjust the labels to almost 100% right-justified.
-         adj = 0.965,
-         ## Increase label size.
-         cex = 1.2)
-    #boxplot(log(df[[2]]))
+    # the selected genes are: in input$gene
+    # figure out which are selected first
+    melted_df = melt(df[df$Gene == input$gene,])
+    ggplot(melted_df, 
+           aes(x=variable,y=log(value),
+               group=Gene,
+               col=Gene, 
+               linetype=Gene)) + 
+      geom_line(size = 1.5)  +
+      scale_linetype_manual(values = c(rep("solid", 10), 
+                                       rep("dotted", 10)
+                                       )
+                            ) +
+      scale_color_manual(values = c(brewer.pal(10,"Spectral"),
+                                    brewer.pal(10,"Spectral")
+                                    )
+                         ) +
+      theme_bw()
     })
 }
 
