4a5b72ab39f3dd94f845a53f2755c92f77c5b7d2
Romario Romain <romarioromain@Romarios-iMac.local>
2020-03-22 20:26:43 -0600

    somewhat linked inputs and outputs

--- 2_app.R
+++ 2_app.R
@@ -2,12 +2,15 @@ library(shiny)
 library(ggplot2)
 library(reshape2)
 library(datasets)
+library(dplyr)
 
 # goi_counts<- read.csv("DEseq2 means_goi_ncounts.csv")
 # rownames(goi_counts)<-goi_counts$Gene
 #                          )
 #goi_counts
 
+df<- read.csv("DEseq2 means_goi_ncounts.csv")
+print(str(df))
 
 ui<- fluidPage(
   titlePanel("ARPE19 progression"),
@@ -23,14 +26,15 @@ ui<- fluidPage(
       #                    label = " Choose Gene of Interest(s)",
       #                    choices =  c("PPP1CB", "PPP2R5C", "BUB1B","AURKB" ,"SKA1"),
       #                    selected = "PPP1CB" ),
-      selectizeInput(inputId = "Sample", 
-                         label = " Choose sample(s)",
-                         choices= c("ARPE19", "T53D4", "RasV12","Aktmyr" ,"MekDD"),
-                         selected = "ARPE19" ),
-      selectizeInput(inputId = "Gene",
-                         label = " Choose Gene of Interest(s)",
-                         choices =  c("PPP1CB", "PPP2R5C", "BUB1B","AURKB" ,"SKA1"),
-                         selected = "PPP1CB" )
+      checkboxGroupInput(inputId = "gene", 
+                         label = " Choose Gene of Interest",
+                         unlist(strsplit('AURKB BUB1 BUB1B NUF2 PPP1CA PPP1CB PPP1CC PPP2R2A PPP2R2A.1 PPP2R5A PPP2R5B PPP2R5C PPP2R5D PPP2R5E SGO1 SKA1 SKA2',
+                                         split=' ')
+                         )
+      ),
+      checkboxGroupInput(inputId = "sample", label = " Choose sample",
+                         c("ARPE19", "T53D4", "RasV12","Aktmyr" ,"MekDD")
+      ),
       # submitButton(text = "Apply Changes", 
       #              icon = NULL,
       #              width = NULL),
@@ -38,7 +42,7 @@ ui<- fluidPage(
     ),
     mainPanel(
       plotOutput("boxplot1"),
-      tableOutput("contents"),
+      tableOutput("contents")
     )
   )
 )
@@ -46,34 +50,49 @@ ui<- fluidPage(
 
 server<- function(input,output, session) {
   session$onSessionEnded(stopApp)
+  ({
     output$contents<- renderTable({
-      in_counts<-input$counts
-      
-      df<-read.csv("DEseq2 means_goi_ncounts.csv")
+      in_counts<-input$counts 
+      df<-read.csv(input$counts$datapath
+    )
     })
     output$boxplot1<- renderPlot({
-      req(input$counts)
-    melted = melt(df)
-    boxplot(melted$value ~ melted$Gene, xaxt="n")
-    text(x = 1:length(levels(melted$Gene)),
-         ## Move labels to just below bottom of chart.
-         y = par("usr")[3] - 0.90,
-         ## Use names from the data list.
-         labels = levels(melted$Gene),
-         ## Change the clipping region.
-         xpd = NA,
-         ## Rotate the labels by 35 degrees.
-         srt = 35,
-         ## Adjust the labels to almost 100% right-justified.
-         adj = 0.965,
-         ## Increase label size.
-         cex = 1)
+      filtered <-
+        df %>%
+        filter(Gene == input$Gene,
+                 ARPE19 == input$ARPE19,
+                 T53D4 == input$T53D4,
+                 RasV12 == input$RasV12,
+                 MekDD == input$MekDD,
+                 Aktmyr == input$Aktmyr
+        )
+      ggplot(filtered, aes(counts))+
+        geom_boxplot()
     })
+    # output$boxplot1<- renderPlot({
+    #   req(input$counts)
+    # melted = melt(df)
+    # boxplot(melted$value ~ melted$Gene, xaxt="n")
+    # text(x = 1:length(levels(melted$Gene)),
+    #      ## Move labels to just below bottom of chart.
+    #      y = par("usr")[3] - 0.90,
+    #      ## Use names from the data list.
+    #      labels = levels(melted$Gene),
+    #      ## Change the clipping region.
+    #      xpd = NA,
+    #      ## Rotate the labels by 35 degrees.
+    #      srt = 35,
+    #      ## Adjust the labels to almost 100% right-justified.
+    #      adj = 0.965,
+    #      ## Increase label size.
+    #      cex = 1)
+    # })
     # output$ARPE19plot<- renderPlot({
     #   boxplot(as.formula(formulaText)),
     #   data= df
     # })
-  }
+     })
+    }
 
-shinyApp (ui, server)
+shinyApp (ui=ui, server=server)
 
